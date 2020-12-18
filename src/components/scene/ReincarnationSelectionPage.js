759cf0891e287baf6e963ccdf578b08742baea22
Damie <damienmarble@gmail.com>
2020-12-17 19:07:59 -0500

    Fix logic for display post-reincarnate traits.

--- src/components/scene/ReincarnationSelectionPage.js
+++ src/components/scene/ReincarnationSelectionPage.js
@@ -1,5 +1,5 @@
 import Grid from "@material-ui/core/Grid";
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import {Creatures} from "../../data/creatures";
 import Button from "@material-ui/core/Button";
 import {getCharacter, getGlobalState, getLevelForPower, getSpriteForCreature} from "../../engine";
@@ -7,11 +7,17 @@ import Tooltip from "@material-ui/core/Tooltip";
 import {Traits} from "../../data/Traits";
 import * as _ from "lodash";
 import { useHistory } from "react-router-dom";
+import {Big} from "big.js";
 
 export default function ReincarnationSelectionPage(props) {
     const globalState = useRef(getGlobalState());
     const history = useHistory();
     const player = getCharacter(0);
+    const currentCreature = Creatures[player.appearance] || {};
+
+    useEffect(() => {
+        getGlobalState().paused = true;
+    }, []);
 
     return <Grid container>
         <Grid item xs={12} style={{textAlign: "center"}}>
@@ -23,12 +29,13 @@ export default function ReincarnationSelectionPage(props) {
             You will reincarnate with <strong>{player.powerLevel.minus(1).toFixed()}</strong> additional starting energy, letting you start at level { getLevelForPower(globalState.current.startingPower.plus(player.powerLevel)).toFixed()}
             <br/>
             You will also gain the following Traits as a result of your previous reincarnations:
-            <ul>
-            {_.uniq(Object.keys(globalState.current.startingTraits).concat(Object.keys(player.traits)))
+            <Grid container>
+            {_.uniq(Object.keys(globalState.current.startingTraits).concat(currentCreature.traits || []))
                 .map(trait => {
                     const player = getCharacter(0);
-                    const combinedLevel = _.get(globalState.current.startingTraits, trait, 0) + _.get(player.traits, trait, 0);
-                    return <li>
+                    const currentStartingRank = _.get(globalState.current.startingTraits, trait, Big(0));
+                    const combinedLevel = player.powerLevel.gt(currentStartingRank) ? player.powerLevel : currentStartingRank;
+                    return <Grid item xs={1}>
                         <Tooltip title={<div dangerouslySetInnerHTML={{__html:
                             Traits[trait].description({
                                 rank: combinedLevel.toFixed()
@@ -36,18 +43,21 @@ export default function ReincarnationSelectionPage(props) {
                         }}></div>}>
                             <img src={Traits[trait].icon}/>
                         </Tooltip>
-                    </li>
+                    </Grid>
                 })}
-            </ul>
+            </Grid>
         </Grid>
 
         <Grid container item xs={12} alignItems="stretch" justify="flex-start">
+            <Grid item xs={12}>
+                <strong>Choose a demon to reincarnate as.</strong>
+            </Grid>
             {
                 Object.keys(Creatures).map(name => {
                     if(!getGlobalState().unlockedMonsters[name]) {
                         return <Grid item container xs={3} justify="space-around" style={{height: "138px"}}>
                             <Grid item xs={12} style={{textAlign: "center", height: "64%"}}>
-                                <Tooltip title={<div>{Creatures[name].description}</div>}>
+                                <Tooltip title={<div>An unknown type of Demon. Selects a random Demon you have not already played as.</div>}>
                                     <Button variant="contained" style={{height: "100%", width: "50%"}} onClick={() => {
                                         props.reincarnate("random");
                                         history.push("/adventuring");
