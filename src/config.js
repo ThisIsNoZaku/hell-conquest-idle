dae56a5a72a57dc13b6d0564c5ed33534d31a9a9
Damie <damienmarble@gmail.com>
2020-12-18 21:40:49 -0500

    Add attribute descriptions and icons.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 console.log(process.env.REACT_APP_DEBUG_MODE, typeof process.env.REACT_APP_DEBUG_MODE);
 
 export const config = {
@@ -15,22 +16,35 @@ export const config = {
         lesserLevelScale: 5, // A demon is "lesser" than another when it's level is this much lower.
         greaterLevelScale: 2 // A demon is "greater" than another when it's level is this much higher.
     },
+    characters: {
+        player: {
+            attributesPerLevel: 2
+        }
+    },
     attributes: {
         brutality: {
             label: "Brutality",
-            globalScaling: 1
+            globalScaling: 1,
+            description: _.template("Brutality is how savage and ruthless a Demon is. It gives a ${rank}% bonus to attack damage, defense and intimidation checks."),
+            icon: "icons/icons-92.png"
         },
         cunning: {
             label: "Cunning",
-            globalScaling: 1
+            globalScaling: 1,
+            description: _.template("Cunning is how quick thinking a Demon is. It gives a ${rank}% bonus to Evasion, and non-combat encounters."),
+            icon: "icons/icons-24.png"
         },
         deceit: {
             label: "Deceit",
             globalScaling: 1,
+            description: _.template("Deceit is how underhanded and manipulative a Demon is. It gives a ${rank}% bonus to Accuracy and social encounters."),
+            icon: "icons/icons-17.png"
         },
         madness: {
             label: "Madness",
-            globalScaling: 1
+            globalScaling: 1,
+            description: _.template("Madness is how disconnected from the limits of reality the Demon is. It gives a ${rank}% bonus to the effect of wielded Artifacts."),
+            icon: "icons/icons-124.png"
         }
     },
     combat: {
