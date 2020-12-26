d19ce7f1fce979cbbc75970b0a9d43ac23049da8
Damie <damienmarble@gmail.com>
2020-12-26 12:49:42 -0500

    Fixed logic for reincarnation enabled.

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -1,7 +1,8 @@
 import Paper from "@material-ui/core/Paper";
-import React from "react";
+import React, {useMemo} from "react";
 import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
+import {getCharacter, getGlobalState, getLevelForPower, getPowerNeededForLevel} from "../engine";
 
 const styles = {
     root: {
@@ -16,8 +17,12 @@ const styles = {
 
 export default function TopSection(props) {
     const history = useHistory();
+    const reincarnateEnabled = useMemo(() => getLevelForPower(getGlobalState().startingPower).lt(getCharacter(0).powerLevel), [
+        getGlobalState().powerLevel,
+        getCharacter(0).powerLevel
+    ])
     return <div style={styles.root}>
-        <Button onClick={() => history.push("/")} style={styles.buttons} variant="contained" color="secondary" disabled={props.character.powerLevel.lte(1)}>
+        <Button onClick={() => history.push("/")} style={styles.buttons} variant="contained" color="secondary" disabled={!reincarnateEnabled}>
             Reincarnate
         </Button>
     </div>
