795eb1da4164978942a7ee7cbfb56a5377ed10b5
Damie <damienmarble@gmail.com>
2021-01-03 01:04:54 -0500

    Reimplemented damage calculation.

--- src/engine/combat/calculateDamageBy.js
+++ src/engine/combat/calculateDamageBy.js
@@ -5,10 +5,10 @@ import { config } from "../../config";
 export default function calculateDamageBy(attacker) {
     return {
         against: function (target) {
-            const damageMultiplier = Decimal(attacker.combat.power
-                .times(config.mechanics.combat.power.effectPerPoint)).plus(100);
-            const damageDivisor = Decimal(_.get(target, ["combat", "resilience"], 0)).times(config.mechanics.combat.power.effectPerPoint).plus(100);
-            const damageModifier = damageMultiplier.div(damageDivisor);
+            const powerMultiplier = Decimal(attacker.combat.power
+                .times(config.mechanics.combat.power.effectPerPoint));
+            const resilienceMultiplier = Decimal(_.get(target, ["combat", "resilience"], 0)).times(config.mechanics.combat.power.effectPerPoint);
+            const damageModifier = powerMultiplier.minus(resilienceMultiplier).plus(1);
             return {
                 min: attacker.combat.minimumDamage.times(damageModifier).ceil(),
                 med: attacker.combat.medianDamage.times(damageModifier).ceil(),
