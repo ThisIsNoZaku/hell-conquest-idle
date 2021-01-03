705e5f68c459e025c10ea961c3454387d0793a41
Damie <damienmarble@gmail.com>
2021-01-03 01:35:43 -0500

    Added hp bonus to madness description.

--- src/data/Attributes.js
+++ src/data/Attributes.js
@@ -19,7 +19,7 @@ export const Attributes = {
     },
     madness: {
         label: "Madness",
-            description: _.template(`Madness is how disconnected from the limits of reality the Demon is. It gives a \${rank * ${ config.mechanics.combat.traitRank.effectPerPoint * 100}}% bonus to the effects of Traits.`),
+            description: _.template(`Madness is how disconnected from the limits of reality the Demon is. It gives a \${rank * ${ config.mechanics.combat.traitRank.effectPerPoint * 100}}% bonus to the effects of Traits and health.`),
             icon: "icons/icons-124.png"
     }
 }
