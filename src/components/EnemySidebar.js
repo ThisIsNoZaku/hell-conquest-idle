69a0f3f63ca7314e574ffd27f54d444da5d798f5
Damie <damienmarble@gmail.com>
2021-01-05 14:53:49 -0500

    Color enemy sheet when they're dead.

--- src/components/EnemySidebar.js
+++ src/components/EnemySidebar.js
@@ -3,14 +3,18 @@ import Paper from "@material-ui/core/Paper";
 import CharacterSheet from "./CharacterSheet";
 
 const styles = {
-    root: {
+    alive: {
         width: "25%",
         backgroundColor: "#eeeeee"
+    },
+    dead: {
+        width: "25%",
+        backgroundColor: "#b3827f"
     }
 }
 
 export default function EnemySidebar(props) {
-    return <Paper style={styles.root}>
+    return <Paper style={!props.enemy || props.enemy.isAlive ? styles.alive : styles.dead}>
         {props.enemy && <CharacterSheet enemy={props.player} character={props.enemy} /> }
     </Paper>
 }
