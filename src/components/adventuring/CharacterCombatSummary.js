a2fb81a31da4d6cf24e143dd0e4be8710e670ed7
Damie <damienmarble@gmail.com>
2021-01-20 01:03:38 -0500

    Changed status tooltips.

--- src/components/adventuring/CharacterCombatSummary.js
+++ src/components/adventuring/CharacterCombatSummary.js
@@ -34,37 +34,17 @@ export default function CharacterCombatSummary(props) {
                 </Tooltip>
             </Grid>
         </Grid>
-        <Grid item xs={12} container direction="row">
-            <Grid item xs>
-                <Tooltip title={`Remaining Accuracy Points: ${props.precisionPoints}/${props.maxPrecisionPoints}`}>
-                    <div style={{alignItems: "center", display: "flex"}}>
-                        <img src="./icons/icons-836.png"/>
-                        <meter min={0} max={props.maxPrecisionPoints} value={props.precisionPoints}
-                               style={{width: "100%"}}/>
-                    </div>
-                </Tooltip>
-            </Grid>
-            <Grid item xs>
-                <Tooltip title={`Remaining Evasion Points: ${props.evasionPoints}/${props.maxEvasionPoints}`}>
-                    <div style={{alignItems: "center", display: "flex"}}>
-                        <img src="./icons/icons-65.png"/>
-                        <meter min={0} max={props.maxEvasionPoints} value={props.evasionPoints}
-                               style={{width: "100%"}}/>
-                    </div>
-                </Tooltip>
-            </Grid>
-        </Grid>
         <Grid container item xs={12} style={{height: "40px"}} direction={props.direction}>
             {props.statuses && Object.keys(props.statuses).map(status => {
                 const numStacks = props.statuses[status].reduce((highest, next) => {
                     return Decimal.max(highest, next.stacks);
                 }, 0);
                 const modifiers = Object.keys(Statuses[status].effects).reduce((combined, next) => {
-                    combined[next] = Decimal.abs(Decimal(1).minus(Decimal(1).plus(Decimal(Statuses[status].effects[next])).pow(numStacks)).times(100));
+                    combined[next] = Decimal.abs(Decimal(1).minus(Decimal(1).plus(Decimal(Statuses[status].effects[next].modifier)).pow(numStacks)).times(100));
                     return combined;
                 }, {});
                 return <Grid item xs={1}>
-                    <Tooltip title={Statuses[status].description(modifiers)}>
+                    <Tooltip title={<div dangerouslySetInnerHTML={{__html: `<strong>${Statuses[status].name}</strong>: ${Statuses[status].description(modifiers)}`}}/>} >
                         <img src={Statuses[status].icon}/>
                     </Tooltip>
                 </Grid>
