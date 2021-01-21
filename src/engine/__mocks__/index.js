fccd71a0e4ce24ddd05b8c5b8d40cab3002de490
Damie <damienmarble@gmail.com>
2021-01-21 09:26:33 -0500

    Updated mocks.

--- src/engine/__mocks__/index.js
+++ src/engine/__mocks__/index.js
@@ -1,7 +1,12 @@
-const globalState = {
-    characters: {},
-    unlockedTraits: {}
-};
-export const getGlobalState = jest.fn().mockReturnValue(globalState);
+import {Character} from "../../character";
+import {Decimal} from "decimal.js";
+
+export const getGlobalState = jest.fn();
+
+export const getCharacter = jest.fn();
 
 export const reincarnateAs = jest.fn();
+
+export const saveGlobalState = jest.fn();
+
+export const generateCreature = jest.fn();
