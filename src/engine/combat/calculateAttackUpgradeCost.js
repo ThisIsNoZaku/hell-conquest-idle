5741bc6e2a46407ab7128098385406fa06cd2cf7
Damie <damienmarble@gmail.com>
2021-01-21 09:55:23 -0500

    In case traits don't affect upgrade/downgrade.

--- src/engine/combat/calculateAttackUpgradeCost.js
+++ src/engine/combat/calculateAttackUpgradeCost.js
@@ -7,7 +7,7 @@ export default function calculateAttackUpgradeCost(attacker, target) {
     const baseCost = attacker.combat.attackUpgradeCost; // Inner calculation already includes its own traits/statuses.
     const targetTraitMultiplier = Object.keys(target.traits).reduce((previousValue, currentValue) => {
         const effectDef = _.get(Traits[currentValue], ["continuous", "trigger_effects", "attack_upgrade_cost_multiplier"]);
-        const effectModifier = effectDef.target === "source_character" ? evaluateExpression(effectDef.modifier, {
+        const effectModifier = _.get(effectDef, "target") === "source_character" ? evaluateExpression(effectDef.modifier, {
             rank: Decimal(target.traits[currentValue])
         }) : 0;
         return previousValue.plus(effectModifier)
