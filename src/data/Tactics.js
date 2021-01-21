d621fe1f68cce0ec843bfae79e8c0197475234a0
Damie <damienmarble@gmail.com>
2021-01-21 15:03:59 -0500

    Add strategies for stamina floors and increase deceptive cost increase.

--- src/data/Tactics.js
+++ src/data/Tactics.js
@@ -6,6 +6,9 @@ export const Tactics = {
             power_modifier: .25,
             health_modifier: .25,
             attack_upgrade_cost_multiplier: .75
+        },
+        strategy: {
+            attack_floor: .1 // Don't spend stamina to upgrade attacks at/below this %
         }
     },
     defensive: {
@@ -15,6 +18,9 @@ export const Tactics = {
             resilience_modifier: .25,
             fatigue_multiplier: .75,
             attack_downgrade_cost_multiplier: .75
+        },
+        strategy: {
+            attack_floor: .25 // Don't spend stamina to upgrade attacks at/below this %
         }
     },
     deceptive: {
@@ -23,7 +29,11 @@ export const Tactics = {
         modifiers: {
             precision_modifier: .25,
             evasion_modifier: .25,
-            always_downgrade_to_glancing: true
+            always_downgrade_to_glancing: true,
+            attack_downgrade_cost_multiplier: 1.25
+        },
+        strategy: {
+            attack_floor: .25 // Don't spend stamina to upgrade attacks at/below this %
         }
     }
 }
