8f6d73bc134f6266eab17ea7cf2c7460fc284c07
Damie <damienmarble@gmail.com>
2021-01-21 21:29:04 -0500

    Fixed the argument order.

--- src/engine/combat/calculateAttackDowngradeCost.js
+++ src/engine/combat/calculateAttackDowngradeCost.js
@@ -4,19 +4,19 @@ import {Decimal} from "decimal.js";
 import * as _ from "lodash";
 import {Statuses} from "../../data/Statuses";
 
-export default function calculateAttackDowngradeCost(attacker, target) {
-    const baseCost = _.get(target, ["combat","incomingAttackDowngradeCost"], Decimal(100)); // Inner calculation already includes its own traits/statuses.
-    const targetTraitMultiplier = Object.keys(_.get(target, "traits", {})).reduce((previousValue, currentValue) => {
+export default function calculateAttackDowngradeCost(character, enemy) {
+    const baseCost = _.get(character, ["combat","incomingAttackDowngradeCost"], Decimal(100)); // Inner calculation already includes its own traits/statuses.
+    const targetTraitMultiplier = Object.keys(_.get(enemy, "traits", {})).reduce((previousValue, currentValue) => {
         const effectDef = _.get(Traits[currentValue], ["continuous", "trigger_effects", "attack_downgrade_cost_multiplier"]);
         const effectModifier = _.get(effectDef, "target") === "enemy" ? evaluateExpression(effectDef.modifier, {
-            rank: Decimal(target.traits[currentValue])
+            rank: Decimal(enemy.traits[currentValue])
         }) : 0;
         return previousValue.plus(effectModifier)
     }, Decimal(1));
-    const targetStatusMultiplier = Object.keys(_.get(target, "statuses", {})).reduce((previousValue, currentValue) => {
+    const targetStatusMultiplier = Object.keys(_.get(enemy, "statuses", {})).reduce((previousValue, currentValue) => {
         const effectDef = _.get(Statuses[currentValue], ["effects", "attack_downgrade_cost_multiplier"]);
         const effectModifier = _.get(effectDef, "target") === "enemy" ? evaluateExpression(effectDef.modifier, {
-            rank: Decimal(target.getStatusStacks(currentValue))
+            rank: Decimal(enemy.getStatusStacks(currentValue))
         }) : 0;
         return previousValue.plus(effectModifier);
     }, Decimal(1));
--- src/engine/combat/calculateAttackUpgradeCost.js
+++ src/engine/combat/calculateAttackUpgradeCost.js
@@ -4,19 +4,19 @@ import {Decimal} from "decimal.js";
 import * as _ from "lodash";
 import {Statuses} from "../../data/Statuses";
 
-export default function calculateAttackUpgradeCost(attacker, target) {
-    const baseCost = _.get(attacker, ["combat","attackUpgradeCost"], Decimal(100)); // Inner calculation already includes its own traits/statuses.
-    const targetTraitMultiplier = Object.keys(_.get(target, "traits", {})).reduce((previousValue, currentValue) => {
+export default function calculateAttackUpgradeCost(character, enemy) {
+    const baseCost = _.get(character, ["combat","attackUpgradeCost"], Decimal(100)); // Inner calculation already includes its own traits/statuses.
+    const targetTraitMultiplier = Object.keys(_.get(enemy, "traits", {})).reduce((previousValue, currentValue) => {
         const effectDef = _.get(Traits[currentValue], ["continuous", "trigger_effects", "attack_upgrade_cost_multiplier"]);
-        const effectModifier = _.get(effectDef, "target") === "source_character" ? evaluateExpression(effectDef.modifier, {
-            rank: Decimal(target.traits[currentValue])
+        const effectModifier = _.get(effectDef, "target") === "enemy" ? evaluateExpression(effectDef.modifier, {
+            rank: Decimal(enemy.traits[currentValue])
         }) : 0;
         return previousValue.plus(effectModifier)
     }, Decimal(1));
-    const targetStatusMultiplier = Object.keys(_.get(target, "statuses", {})).reduce((previousValue, currentValue) => {
+    const targetStatusMultiplier = Object.keys(_.get(enemy, "statuses", {})).reduce((previousValue, currentValue) => {
         const effectDef = _.get(Statuses[currentValue], ["effects", "attack_upgrade_cost_multiplier"]);
-        const effectModifier = _.get(effectDef, "target") === "self" ? evaluateExpression(effectDef.modifier, {
-            rank: Decimal(target.getStatusStacks(currentValue))
+        const effectModifier = _.get(effectDef, "target") === "enemy" ? evaluateExpression(effectDef.modifier, {
+            rank: Decimal(enemy.getStatusStacks(currentValue))
         }) : 0;
         return previousValue.plus(effectModifier);
     }, Decimal(1));
