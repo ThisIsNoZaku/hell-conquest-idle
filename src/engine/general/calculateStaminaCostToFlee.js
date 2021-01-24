176f969925db2ba4058c753ab4cf4e8f13269365
Damie <damienmarble@gmail.com>
2021-01-23 23:53:37 -0500

    Reduce flee stamina cost from deceit.

--- src/engine/general/calculateStaminaCostToFlee.js
+++ src/engine/general/calculateStaminaCostToFlee.js
@@ -3,6 +3,7 @@ import {getConfigurationValue} from "../../config";
 
 export default function calculateStaminaCostToFlee(player, enemy) {
     const baseCost = Decimal(getConfigurationValue("flee_stamina_cost_base"))
-            .times(Decimal.max(0, enemy.powerLevel.minus(player.powerLevel)));
+            .times(Decimal.max(0, enemy.powerLevel.minus(player.powerLevel)))
+        .times(Decimal(1).minus(Decimal(player.attributes["deceit"]).times(.05))); // FIXME: Move values into configuration
     return baseCost;
 }
