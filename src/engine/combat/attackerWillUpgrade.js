97908ff3982b2a7758bd34d6e52e57ec0d2efa04
Damie <damienmarble@gmail.com>
2021-01-25 18:32:36 -0500

    Upgrade based on total instead of marginal cost.

--- src/engine/combat/attackerWillUpgrade.js
+++ src/engine/combat/attackerWillUpgrade.js
@@ -3,10 +3,11 @@ import {HitTypes} from "../../data/HitTypes";
 import {getConfigurationValue} from "../../config";
 
 export default function attackerWillUpgrade(attacker, defender, upgradeCost, currentHitLevel, timesUpgraded) {
-    const attackerHasMoreStamina = attacker.combat.stamina.gt(defender.combat.stamina);
+    const totalUpgradeCost = upgradeCost.times(timesUpgraded + 1);
+    const attackerHasMoreStamina = attacker.combat.stamina.minus(totalUpgradeCost).gt(defender.combat.stamina);
     const attackerWantsTo = Tactics[attacker.tactics].strategy.attack === "always" ||
         attackerHasMoreStamina;
-    return upgradeCost.times(1 + timesUpgraded).lte(attacker.combat.stamina) &&
+    return totalUpgradeCost.lte(attacker.combat.stamina) &&
         ((currentHitLevel !== HitTypes.max && attackerWantsTo && timesUpgraded < getConfigurationValue("maximum_upgrade_times")) ||
             (attacker.combat.stamina.eq(attacker.combat.maximumStamina) && timesUpgraded === 0));
 }
