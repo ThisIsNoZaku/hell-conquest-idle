d1630139cfefda1055c41121d7cc81f8c826606f
Damie <damienmarble@gmail.com>
2021-01-25 18:32:11 -0500

    Always downgrade if less health.

--- src/engine/combat/defenderWillDowngrade.js
+++ src/engine/combat/defenderWillDowngrade.js
@@ -3,11 +3,13 @@ import {Tactics} from "../../data/Tactics";
 import {getConfigurationValue} from "../../config";
 
 export default function defenderWillDowngrade(attacker, defender, downgradeCost, currentHitLevel, timesDowngraded) {
+    const totalDowngradeCost = downgradeCost.times(timesDowngraded + 1) ;
     const defenderHasMoreStamina = defender.combat.stamina.gt(attacker.combat.stamina);
     const hitIsCurrentlyMaxLevel = currentHitLevel === HitTypes.max;
     const defenderWantsTo = Tactics[defender.tactics].strategy.defend === "always" ||
         (Tactics[defender.tactics].strategy.defend === "upgraded" && hitIsCurrentlyMaxLevel) ||
-        (Tactics[defender.tactics].strategy.defend === "advantage" && defenderHasMoreStamina);
-    return downgradeCost.times(1 + timesDowngraded).lte(defender.combat.stamina) && currentHitLevel !== HitTypes.min && defenderWantsTo &&
+        (Tactics[defender.tactics].strategy.defend === "advantage" && defenderHasMoreStamina) ||
+        defender.hp.lt(attacker.hp);
+    return totalDowngradeCost.lte(defender.combat.stamina) && currentHitLevel !== HitTypes.min && defenderWantsTo &&
         timesDowngraded < getConfigurationValue("maximum_downgrade_times");
 }
