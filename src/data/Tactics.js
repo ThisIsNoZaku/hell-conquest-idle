fe4836e1b8896f2596b04b25c994614e2e019304
Damie <damienmarble@gmail.com>
2021-01-25 18:33:04 -0500

    Removed stat modifiers for tactics.

--- src/data/Tactics.js
+++ src/data/Tactics.js
@@ -3,7 +3,7 @@ export const Tactics = {
         title: "Aggressive",
         description: "Your tactics emphasize overwhelming power and violence.",
         modifiers: {
-            attack_upgrade_cost_multiplier: -.25
+            // attack_upgrade_cost_multiplier: -.25
         },
         strategy: {
             attack: "always",
@@ -14,7 +14,7 @@ export const Tactics = {
         title: "Defensive",
         description: "Your tactics emphasize caution and husbanding your strength.",
         modifiers: {
-            solid_hit_received_damage_multiplier: -.25
+            // solid_hit_received_damage_multiplier: -.25
         },
         strategy: {
             attack: "advantage",
@@ -26,7 +26,8 @@ export const Tactics = {
         description: "Your tactics emphasize disorientation and trickery.",
         modifiers: {
             downgrade_devastating_to_miss: true,
-            attack_upgrade_cost_multiplier: .25
+            // attack_upgrade_cost_multiplier: .25,
+            // attack_downgrade_cost_multiplier: .25
         },
         strategy: {
             attack: "advantage",
