2c848c6c2b81aa0dff6a17c99764777f7a3a563d
Damie <damienmarble@gmail.com>
2021-01-26 12:50:06 -0500

    Change level cap and trait tier up.

--- src/config.js
+++ src/config.js
@@ -29,14 +29,14 @@ const config = {
     minimum_stamina: 400,
     bonus_stamina_per_level: 100,
     intimidation_cost_attribute: "cunning",
-    trait_tier_up_levels: 20, // Traits upgrade when reaching a multiple of this level.,
+    trait_tier_up_levels: 25, // Traits upgrade when reaching a multiple of this level.
     minimum_attack_upgrade_cost: 10,
     minimum_attack_downgrade_cost: 10,
     maximum_upgrade_times: 2,
     maximum_downgrade_times: 1,
     fatigue_penalty_per_point: 25,
     stamina_recovery_per_level: 50,
-    game_level_cap: 11,
+    game_level_cap: 26,
     mechanics: {
         reincarnation: {
             latentPowerModifier: "Decimal.min(latentPower, highestLevelReached).plus(latentPower.minus(Decimal.min(latentPower, highestLevelReached).sqrt())",
