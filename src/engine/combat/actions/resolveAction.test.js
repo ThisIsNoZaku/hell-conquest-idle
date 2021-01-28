dda468cb8203babaa122beec5a779bca4aaeed97
Damie <damienmarble@gmail.com>
2021-01-28 01:58:56 -0500

    Updated action tests.

--- src/engine/combat/actions/resolveAction.test.js
+++ src/engine/combat/actions/resolveAction.test.js
@@ -9,34 +9,48 @@ describe("resolve action", function () {
         player = new Character({
             id: 0,
             tactics: {
-                offensive: "overwhelming"
+                offensive: "overwhelm"
             }
         });
         enemy = new Character({
             id: 1,
-            tactics: "defensive"
+            tactics: {
+                defensive: "none"
+            }
         });
     })
-    it("performs an attack if acting character has Overwhelming tactics", function () {
+    it("performs a power attack if acting character has Overwhelming tactics", function () {
         const roundEvents = [];
+        player.combat.stamina = Decimal(1000);
         resolveAction(player, {0: player, 1: enemy}, roundEvents, 100);
         expect(roundEvents).toEqual([
             {
                 event: "attack",
-                hitType: 0,
+                hitType: 1,
                 children: [
                     expect.any(String)
                 ],
                 source: {
                     character: 0
                 },
+                action: "powerAttack",
+                actionEnergyCost: Decimal(25 * .9 * 1.5).floor(),
+                reaction: "none",
+                reactionEnergyCost: Decimal(0).floor(),
                 target: 1,
+                uuid: expect.any(String),
                 hit: true,
             },
             {
                 event: "damage",
                 type: "physical",
-                value: Decimal(10),
+                value: Decimal(22),
+                target: 1,
+                source: {
+                    character: 0,
+                    trait: undefined
+                },
+                uuid: expect.any(String),
                 parent: expect.any(String)
             }
         ])
