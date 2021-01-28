207ead8130e9b6f41b1168fd3d3475604a9cee21
Damie <damienmarble@gmail.com>
2021-01-28 16:28:24 -0500

    Fixed attributes modifier.

--- src/engine/combat/actions/calculateActionCost.js
+++ src/engine/combat/actions/calculateActionCost.js
@@ -1,10 +1,11 @@
 import {getConfigurationValue} from "../../../config";
 import calculateAttributeDifferentMultiplier from "../calculateAttributeDifferentMultiplier";
 import {AttackActions} from "../../../data/CombatActions";
+import {Decimal} from "decimal.js";
 
 export default function calculateActionCost(actor, action, target) {
     const base = target.powerLevel.times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
-    const attributeMultiplier = actor.combat.precision.times(getConfigurationValue("mechanics.combat.precision.effectPerPoint")).plus(1);
+    const attributeMultiplier = Decimal(1).minus(actor.combat.precision.times(getConfigurationValue("mechanics.combat.precision.effectPerPoint")));
     const actionCostMultiplier = AttackActions[action].energyCostMultiplier;
 
     return base.times(attributeMultiplier)
