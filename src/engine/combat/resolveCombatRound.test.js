60e8d19fc7edd8da6ec97e5aecaae449d38487be
Damie <damienmarble@gmail.com>
2021-01-28 02:26:52 -0500

    Resolve combat round tests.

--- src/engine/combat/resolveCombatRound.test.js
+++ src/engine/combat/resolveCombatRound.test.js
@@ -1,40 +1,76 @@
-import resolveCombatRound from "./resolveCombatRound";
-import Decimal from "decimal.js";
-import {HitTypes} from "../../data/HitTypes";
+import triggerEvent from "../general/triggerEvent";
+import {resolveCombatRound} from "./index";
 import {Character} from "../../character";
-import {getConfigurationValue} from "../../config";
-import {Tactics} from "../../data/Tactics";
+import resolveAction from "./actions/resolveAction";
 
 jest.mock("../index");
+jest.mock("../general/triggerEvent");
+jest.mock("../combat/actions/resolveAction");
 
-// describe('The combat round resolution', function () {
-//     let player;
-//     let enemy;
-//     beforeEach(() => {
-//         player = new Character({
-//             isPc: true,
-//             id: 0,
-//             hp: Decimal(50),
-//             tactics: "defensive",
-//             powerLevel: 1,
-//             attributes: {
-//                 baseBrutality: 1,
-//                 baseCunning: 1,
-//                 baseDeceit: 1,
-//                 baseMadness: 1
-//             }
-//         }, 0);
-//         enemy = new Character({
-//             id: 1,
-//             hp: Decimal(50),
-//             powerLevel: 1,
-//             tactics: "defensive",
-//             attributes: {
-//                 baseBrutality: 1,
-//                 baseCunning: 1,
-//                 baseDeceit: 1,
-//                 baseMadness: 1
-//             }
-//         }, 1);
-//     });
-// });
+describe("the combat round resolution", function () {
+    let player;
+    let enemy;
+    beforeEach(() => {
+        player = new Character({
+            id: 0
+        });
+        enemy = new Character({
+            id: 1
+        });
+    })
+    it("calls onRoundBegin for each combatant", function() {
+        resolveCombatRound(100, {0: player, 1: enemy});
+        expect(triggerEvent).toHaveBeenCalledWith({
+            type: "on_round_begin",
+            source: {
+                character: player
+            },
+            combatants: {
+                0: player,
+                1: enemy
+            },
+            roundEvents: expect.any(Array)
+        });
+        expect(triggerEvent).toHaveBeenCalledWith({
+            type: "on_round_begin",
+            source: {
+                character: enemy
+            },
+            combatants: {
+                0: player,
+                1: enemy
+            },
+            roundEvents: expect.any(Array)
+        })
+    });
+    it("calls resolveAction for each combatant", function() {
+        resolveCombatRound(100, {0: player, 1: enemy});
+        expect(resolveAction).toHaveBeenCalledWith(player, {0: player, 1: enemy}, expect.any(Array), 100);
+        expect(resolveAction).toHaveBeenCalledWith(enemy, {0: player, 1: enemy}, expect.any(Array), 100);
+    });
+    it("calls onRoundEnd for each combatant", function() {
+        resolveCombatRound(100, {0: player, 1: enemy});
+        expect(triggerEvent).toHaveBeenCalledWith({
+            type: "on_round_end",
+            source: {
+                character: player
+            },
+            combatants: {
+                0: player,
+                1: enemy
+            },
+            roundEvents: expect.any(Array)
+        });
+        expect(triggerEvent).toHaveBeenCalledWith({
+            type: "on_round_end",
+            source: {
+                character: enemy
+            },
+            combatants: {
+                0: player,
+                1: enemy
+            },
+            roundEvents: expect.any(Array)
+        })
+    });
+});
