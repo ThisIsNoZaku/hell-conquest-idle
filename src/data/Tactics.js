64a93229999dd69347e003231d744723045be301
Damie <damienmarble@gmail.com>
2021-01-29 17:05:07 -0500

    Make Tactics more complex.

--- src/data/Tactics.js
+++ src/data/Tactics.js
@@ -3,34 +3,52 @@ export const Tactics = {
         overwhelm: {
             title: "Overwhelming",
             description: "",
-            strategy: "power"
+            strategy: "power",
+            actions: ["acidSpit", "fireBlast", "powerAttack"],
+            energyCeiling: 0, // Always spend energy to perform actions if energy is above this percentage.
+            energyFloor: 0
         },
         attrit: {
             title: "Attrition",
             description: "",
-            strategy: "attrition"
+            strategy: "attrition",
+            actions: ["arcaneShield", "acidSpit", "fireBlast", "basicAttack"],
+            energyCeiling: .8, // Always spend energy to perform actions if energy is above this percentage.
+            energyFloor: .5 // Don't spend energy to perform actions if energy is below this percentage
         },
         counter: {
             title: "Counter",
             description: "",
-            strategy: "counter"
+            strategy: "counter",
+            actions: ["acidSpit", "fireBlast", "powerAttack", "arcaneShield"],
+            energyCeiling: .75, // Always spend energy to perform actions if energy is above this percentage.
+            energyFloor: .25 // Don't spend energy to perform actions if energy is below this percentage
         }
     },
     defensive: {
         none: {
             title: "No Defense",
             description: "",
-            strategy: "none"
+            strategy: "none",
+            actions: ["block"],
+            energyCeiling: 1, // Always spend energy to perform actions if energy is above this percentage.
+            energyFloor: 1 // Don't spend energy to perform reactions if energy is below this percentage
         },
         block: {
             title: "Block",
             description: "",
-            strategy: "block"
+            strategy: "block",
+            actions: ["block"],
+            energyCeiling: 0,
+            energyFloor: 0,
         },
         dodge: {
             title: "Evade",
             description: "",
-            strategy: "dodge"
+            strategy: "dodge",
+            actions: ["dodge"],
+            energyCeiling: .25,
+            energyFloor: 0
         }
     },
 }
