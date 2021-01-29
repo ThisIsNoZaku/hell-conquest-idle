66aa02959ccdbb315da8008243d916f651c2a13b
Damie <damienmarble@gmail.com>
2021-01-29 17:01:21 -0500

    Implement combat reaction logic.

--- src/engine/combat/actions/determineCharacterCombatReaction.js
+++ src/engine/combat/actions/determineCharacterCombatReaction.js
@@ -1,3 +1,24 @@
-export default function determineCharacterCombatReaction(attacker, attack, target) {
-    return target.tactics.defensive;
+import calculateReactionCost from "./calculateReactionCost";
+import {DefenseActions} from "../../../data/CombatActions";
+import {Tactics} from "../../../data/Tactics";
+import {debugMessage} from "../../../debugging";
+
+export default function determineCharacterCombatReaction(attacker, attack, target, targetStartingEnergy) {
+    debugMessage(`Determining reaction for '${target.id}'`)
+    const actionCosts = Object.keys(DefenseActions).reduce((actions, next)=>{
+        actions[next] = calculateReactionCost(attacker, next, target);
+        return actions;
+    }, {});
+    const energyPercentage = targetStartingEnergy.div(target.combat.maximumStamina);
+    debugMessage(`Energy percentage is ${energyPercentage}`);
+    // TODO: At or over max stamina, use most powerful defense
+    if((energyPercentage.gte(Tactics.defensive[target.tactics.defensive].energyFloor) || energyPercentage.gte(Tactics.defensive[target.tactics.defensive].energyCeiling)) && target.combat.stamina.gte(actionCosts[target.tactics.defensive])) {
+        if(energyPercentage.gte(1)) {
+            debugMessage("At/over energy cap, so dodging.");
+            return "dodge";
+        }
+        debugMessage(`Using defense ${target.tactics.defensive}`);
+        return target.tactics.defensive;
+    }
+    return "none";
 }
