0b60236184abd8fce20f42879029e2151509e3e2
Damie <damienmarble@gmail.com>
2021-01-28 22:58:57 -0500

    Incorporate both attacker and target traits.

--- src/engine/combat/calculateDamageBy.js
+++ src/engine/combat/calculateDamageBy.js
@@ -14,7 +14,7 @@ export default function calculateDamageBy(attacker) {
             debugMessage(`Attacker ${_.get(attacker, "id")} has power ${attackerPower}.`);
             const defenderResilience = Decimal(_.get(target, ["combat", "resilience"], attackerPower));
 
-            if(target) {
+            if (target) {
                 debugMessage(`Defender ${target.id} has resilience ${defenderResilience}.`);
             } else {
                 debugMessage(`No target means an effective resilience of ${defenderResilience}`)
@@ -29,21 +29,29 @@ export default function calculateDamageBy(attacker) {
             return Object.keys(HitTypes).reduce((damage, nextType) => {
                 const perLevelDamage = getConfigurationValue("damage_per_level");
                 const hitTypeDamageMultiplier = HitTypes[nextType].damageMultiplier;
-                const traitDamageMultiplier = Object.keys(_.get(attacker, "traits", {})).reduce((previousValue, currentValue) => {
-                    const traitDamageMultiplier = evaluateExpression(_.get(Traits[currentValue], ["continuous", 'trigger_effects', `${HitTypes[nextType].summary}_hit_damage_multiplier`, "value"], 0), {
-                        tier: Decimal(attacker.traits[currentValue])
-                    });
-                    return previousValue.plus(traitDamageMultiplier)
+                const attackerTraitDamageMultiplier = Object.keys(_.get(attacker, "traits", {})).reduce((previousValue, currentValue) => {
+                    const traitEffectDefinition = _.get(Traits[currentValue], ["continuous", 'trigger_effects', `${HitTypes[nextType].summary}_hit_damage_multiplier`],
+                        _.get(Traits[currentValue], ["continuous", 'trigger_effects', `damage_modifier`]));
+                    const traitApplies = _.get(traitEffectDefinition, ["target"]) === "all" || _.get(traitEffectDefinition, ["target"]) === "self";
+                    return previousValue.plus(traitApplies ? Decimal(traitEffectDefinition.value).times(attacker.traits[currentValue]) : 0);
+                }, Decimal(0));
+                const defenderTraitDamageMultiplier = Object.keys(_.get(target, "traits", {})).reduce((previousValue, currentValue) => {
+                    const traitEffectDefinition = _.get(Traits[currentValue], ["continuous", 'trigger_effects', `${HitTypes[nextType].summary}_hit_damage_multiplier`],
+                        _.get(Traits[currentValue], ["continuous", 'trigger_effects', `damage_modifier`]));
+                    const traitApplies = _.get(traitEffectDefinition, ["target"]) === "all" || _.get(traitEffectDefinition, ["target"]) === "enemy";
+                    return previousValue.plus(traitApplies ? Decimal(traitEffectDefinition.value).times(target.traits[currentValue]) : 0);
                 }, Decimal(0));
                 const attackerTacticsMultiplier = Decimal(0).plus(_.get(Tactics, [_.get(attacker, "tactics"), "modifiers", `${HitTypes[nextType].summary}_hit_damage_multiplier`], 0));
                 const defenderTacticsMultiplier = Decimal(0).plus(_.get(Tactics, [_.get(target, "tactics"), "modifiers", `${HitTypes[nextType].summary}_hit_received_damage_multiplier`], 0));
                 const totalMultiplier = Decimal(attributeDamageMultiplier)
-                    .plus(traitDamageMultiplier)
+                    .plus(attackerTraitDamageMultiplier)
                     .plus(attackerTacticsMultiplier)
-                    .plus(defenderTacticsMultiplier);
+                    .plus(defenderTacticsMultiplier)
+                    .plus(defenderTraitDamageMultiplier);
+                const latentPowerModifier = Decimal(_.get(attacker, "latentPowerModifier", 0)).plus(1);
                 damage[nextType] = Decimal.max(0, attackerPowerLevel.times(perLevelDamage)
                     .times(hitTypeDamageMultiplier)
-                    .times(Decimal(_.get(attacker, "latentPowerModifier", 0)).plus(1))
+                    .times(latentPowerModifier)
                     .times(totalMultiplier))
                     .floor();
                 return damage;
