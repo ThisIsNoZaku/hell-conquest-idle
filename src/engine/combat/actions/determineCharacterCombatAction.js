6047af55996b7c1a173f4b4a120a7ed0e4d6ddf2
Damie <damienmarble@gmail.com>
2021-01-29 17:03:35 -0500

    Tweaked action selection logic.

--- src/engine/combat/actions/determineCharacterCombatAction.js
+++ src/engine/combat/actions/determineCharacterCombatAction.js
@@ -1,37 +1,37 @@
 import {Decimal} from "decimal.js";
-import {getConfigurationValue} from "../../../config";
 import calculateActionCost from "./calculateActionCost";
 import calculateReactionCost from "./calculateReactionCost";
+import {Tactics} from "../../../data/Tactics";
+import {debugMessage} from "../../../debugging";
 
-export default function determineCharacterCombatAction(actingCharacter, enemy) {
-    const attackEnergyCosts = {
-        small: calculateActionCost(actingCharacter, "basicAttack", enemy),
-        power: calculateActionCost(actingCharacter, "powerAttack", enemy),
-        none: Decimal(0)
-    }
+export default function determineCharacterCombatAction(actingCharacter, actorStartingEnergy, enemy) {
+    debugMessage(`Determining action for '${actingCharacter.id}'`)
     const defenseEnergyCosts = {
         none: Decimal(0),
         block: calculateReactionCost(actingCharacter, "block", enemy),
         dodge: calculateReactionCost(actingCharacter, "dodge", enemy)
     }
-    switch (actingCharacter.tactics.offensive) {
-        case "overwhelm":
-            if(attackEnergyCosts.power.lte(actingCharacter.combat.stamina)) {
-                return "powerAttack";
+    // TODO: Over energy ceiling, use most powerful attack
+    const energyPercentage = actorStartingEnergy.div(actingCharacter.combat.maximumStamina);
+    return actingCharacter.attacks.reduce((chosen, next)=>{
+        if(chosen) {
+            return chosen;
+        }
+        const actionCost = calculateActionCost(actingCharacter, next, enemy);
+        const canAffordAction = actionCost.lte(actingCharacter.combat.stamina);
+        const energyPercentAboveFloor = energyPercentage.gte(Tactics.offensive[actingCharacter.tactics.offensive].energyFloor);
+        const belowEnergyFloor = energyPercentage.gte(Tactics.offensive[actingCharacter.tactics.offensive].energyCeiling);
+        if(canAffordAction && energyPercentAboveFloor) {
+            if(actingCharacter.tactics.offensive === "counter" && belowEnergyFloor) {
+                debugMessage()
+                if(defenseEnergyCosts.dodge.gt(enemy.combat.stamina) || (energyPercentage.gte(Tactics.offensive[actingCharacter.tactics.offensive].energyCeiling) && next === "basicAttack")) {
+                    debugMessage(`Enemy can dodge? ${defenseEnergyCosts.dodge.gt(enemy.combat.stamina)}. Energy percentage >= ceiling? ${energyPercentage.gte(Tactics.offensive[actingCharacter.tactics.offensive].energyCeiling)}.`);
+                    return next;
+                }
             } else {
-                return "none"
+                return next;
             }
-        case "attrit":
-            if(attackEnergyCosts.small.lte(actingCharacter.combat.stamina)) {
-                return "basicAttack";
-            } else {
-                return "none"
-            }
-        case "counter":
-            if(defenseEnergyCosts.dodge.gt(enemy.combat.stamina) && attackEnergyCosts.power.lte(actingCharacter.combat.stamina)) {
-                return "powerAttack";
-            } else {
-                return "none";
-            }
-    }
+        }
+        return chosen;
+    }, null) || "none";
 }
