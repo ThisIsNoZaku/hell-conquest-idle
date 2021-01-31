f669796a1a7c88b3615f9cf5ce826fda1923f05c
Damie <damienmarble@gmail.com>
2021-01-31 14:40:22 -0500

    Account for actor/target being undefined.

--- src/engine/combat/actions/calculateActionCost.js
+++ src/engine/combat/actions/calculateActionCost.js
@@ -2,10 +2,10 @@ import {getConfigurationValue} from "../../../config";
 import calculateAttributeDifferentMultiplier from "../calculateAttributeDifferentMultiplier";
 import {AttackActions} from "../../../data/CombatActions";
 import {Decimal} from "decimal.js";
-
+import * as _ from "lodash";
 export default function calculateActionCost(actor, action, target) {
-    const base = target.powerLevel.times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
-    const attributeMultiplier = Decimal(1).minus(actor.combat.precision.times(getConfigurationValue("mechanics.combat.precision.effectPerPoint")));
+    const base = Decimal(_.get(target, "powerLevel", 1)).times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
+    const attributeMultiplier = Decimal(1).minus(Decimal(_.get(actor, ["combat", "precision"], 1)).times(getConfigurationValue("mechanics.combat.precision.effectPerPoint")));
     const actionCostMultiplier = AttackActions[action.primary].energyCostMultiplier;
     const enhancementModifier = action.enhancements.reduce((previousValue, currentValue, currentIndex) => {
         return previousValue + (currentValue.additional_energy_cost_modifier || 0);
--- src/engine/combat/actions/calculateReactionCost.js
+++ src/engine/combat/actions/calculateReactionCost.js
@@ -5,10 +5,10 @@ import {Traits} from "../../../data/Traits";
 import * as _ from "lodash";
 
 export default function calculateReactionCost(actor, reaction, target) {
-    const base = actor.powerLevel.times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
-    const attributeMultiplier = Decimal(1).minus(target.combat.evasion.times(getConfigurationValue("mechanics.combat.evasion.effectPerPoint")));
+    const base = Decimal(_.get(actor, "powerLevel", 1)).times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
+    const attributeMultiplier = Decimal(1).minus(_.get(target, ["combat", "evasion"], Decimal(1)).times(getConfigurationValue("mechanics.combat.evasion.effectPerPoint")));
     const actionCostMultiplier = DefenseActions[reaction.primary].energyCostMultiplier;
-    const attackerTraitModifier = Object.keys(actor.traits).reduce((previousValue, currentValue) => {
+    const attackerTraitModifier = Object.keys(_.get(actor, "traits", {})).reduce((previousValue, currentValue) => {
         const modifier = _.get(Traits[currentValue], ["continuous", "trigger_effects", `${reaction.primary}_cost_modifier`], 0);
         const applies = _.get(modifier, "target") === "any" || _.get(modifier, "target") === "enemy";
         return previousValue.plus(applies ? Decimal(modifier.value).times(actor.traits[currentValue]) : 0);
