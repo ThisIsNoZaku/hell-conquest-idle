7d859986387a44da91f9d7ac44eafe374aabc048
Damie <damienmarble@gmail.com>
2021-02-02 12:17:53 -0500

    Increased reincarnate cap.

--- src/engine/general/reincarnateAs.test.js
+++ src/engine/general/reincarnateAs.test.js
@@ -68,6 +68,6 @@ describe("reincarnateAt", function () {
             baseMadness: 1,
             baseDeceit: 1
         });
-        expect(player.latentPowerCap).toEqual(Decimal(25));
+        expect(player.latentPowerCap).toEqual(Decimal(50));
     });
 })
