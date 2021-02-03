2e6a7f2ccaeca667841a3affbd93da681be8c6d9
Damie <damienmarble@gmail.com>
2021-02-03 16:19:21 -0500

    Properly round and clamp value.

--- src/engine/combat/calculateAttributeDifferentMultiplier.js
+++ src/engine/combat/calculateAttributeDifferentMultiplier.js
@@ -4,10 +4,11 @@ import * as lerp from "lerp";
 
 export default function calculateAttributeDifferentMultiplier(a, b) {
     const multipliers = getConfigurationValue("mechanics.combat.attributeDifferenceMultipliers");
-    const base = Decimal(a).minus(b);
+    const base = Decimal.max(5, Decimal.min(-5, Decimal(a).minus(b)));
     const roundedDown = base.floor();
     const roundedUp = base.ceil();
     const remainder = Math.abs(multipliers[roundedDown.toNumber()] - multipliers[roundedUp.toNumber()]);
-    const multiplier = lerp(multipliers[roundedDown.toNumber()], multipliers[roundedUp.toNumber()], remainder);
-    return Decimal(multiplier).toSignificantDigits(2);
+    const multiplier = Decimal(lerp(multipliers[roundedDown.toNumber()], multipliers[roundedUp.toNumber()], remainder))
+        .times(100).floor().div(100);
+    return multiplier;
 }
