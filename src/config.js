bac663d27441988b9b8d62089cbd68f5bf27485b
Damie <damienmarble@gmail.com>
2021-02-03 14:43:17 -0500

    Removed base stamina but increased per-level max stamina and significantly increased energy costs.

--- src/config.js
+++ src/config.js
@@ -17,16 +17,16 @@ const config = {
     // Combat
     base_attack_upgrade_cost: 0,
     base_attack_downgrade_cost: 0,
-    attack_upgrade_cost_per_enemy_level: 25, // Add this amount times enemy level to upgrade your attacks
-    attack_downgrade_cost_per_enemy_level: 25, // Add this amount times enemy level to downgrade enemy attacks
+    attack_upgrade_cost_per_enemy_level: 100, // Add this amount times enemy level to upgrade your attacks
+    attack_downgrade_cost_per_enemy_level: 100, // Add this amount times enemy level to downgrade enemy attacks
     damage_per_level: 15,
     flee_stamina_cost_base: 25,
     flee_stamina_minimum_cost: 1,
     instant_death_offset: 5,
     recovery_action_healing_percentage: .5,
     recover_action_stamina_percentage: 1,
-    minimum_stamina: 100,
-    bonus_stamina_per_level: 100,
+    minimum_stamina: 0,
+    bonus_stamina_per_level: 250,
     intimidation_cost_attribute: "cunning",
     trait_tier_up_levels: 25, // Traits upgrade when reaching a multiple of this level.
     minimum_attack_upgrade_cost: 10,
@@ -35,7 +35,7 @@ const config = {
     maximum_downgrade_times: 1,
     fatigue_penalty_per_point: 25,
     stamina_recovery_per_level: 50,
-    base_power_generated_per_level_per_tick: .1,
+    base_power_generated_per_level_per_tick: .3,
     power_generation_growth_scaling: .01,
     game_level_cap: 26,
     mechanics: {
