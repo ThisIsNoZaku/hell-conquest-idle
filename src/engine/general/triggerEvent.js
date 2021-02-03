2ddf7a0bcd4f57e113bab89b2689b70fd8740f4c
Damie <damienmarble@gmail.com>
2021-02-03 09:30:27 -0500

    Refactored event triggering.

--- src/engine/general/triggerEvent.js
+++ src/engine/general/triggerEvent.js
@@ -18,40 +18,44 @@ export default function triggerEvent(event) {
     }
     // Trigger traits for event
     debugMessage(`Triggering event ${event.type}`);
-    Object.keys(event.source.character.traits).forEach(traitId => {
-        const trait = Traits[traitId];
-        const eventDefinition = trait[event.type];
-        if (eventDefinition) {
-            const traitTriggered = doesTraitTrigger(eventDefinition, event);
-            debugMessage(`Trait ${traitId} did ${traitTriggered ? '' : 'not'} trigger.`);
-            const effectsToApply = eventDefinition[traitTriggered ? "trigger_effects" : "not_trigger_effects"];
-            if (effectsToApply) {
-                applyTraitEffects(effectsToApply, event, "trait", traitId, event.source.character.traits[traitId]);
+
+    Object.values(event.combatants).forEach(combatant => {
+        Object.keys(_.get(combatant, "traits", {})).forEach(traitId => {
+            const trait = Traits[traitId];
+            const eventDefinition = trait[event.type];
+            if (eventDefinition) {
+                const traitTriggered = doesTraitTrigger(eventDefinition, event, combatant);
+                debugMessage(`Trait ${traitId} did ${traitTriggered ? '' : 'not'} trigger.`);
+                const effectsToApply = eventDefinition[traitTriggered ? "trigger_effects" : "not_trigger_effects"];
+                if (effectsToApply) {
+                    applyTraitEffects(effectsToApply, combatant, event, "trait", traitId, combatant.traits[traitId]);
+                }
             }
-        }
-    });
-    Object.keys(event.source.character.statuses).forEach(statusId => {
-        const status = Statuses[statusId];
-        Object.keys(status.effects).forEach(effect => {
-            switch (effect) {
-                case "inflict_damage_at_start_of_round":
-                    if (event.type !== "on_round_begin") {
-                        return;
-                    }
-                    const targets = selectConditionTargets(status.effects[effect].target, event.source.character, event.target, event.combatants);
-                    targets.forEach(target => {
-                        const activeStatus = target.getActiveStatusInstance(statusId);
-                        const damageToDeal = Decimal(status.effects[effect].value)
-                            .times(target.getStatusStacks(statusId))
-                            .times(getCharacter(activeStatus.source.character).powerLevel)
-                            .floor();
-                        target.dealDamage(damageToDeal);
-                        event.roundEvents.push(generateDamageEvent(getCharacter(activeStatus.source.character), target, damageToDeal, "status", statusId));
-                    })
-                    break;
-            }
+        });
+        Object.keys(_.get(combatant, "statuses", {})).forEach(statusId => {
+            const status = Statuses[statusId];
+            Object.keys(status.effects).forEach(effect => {
+                switch (effect) {
+                    case "inflict_damage_at_start_of_round":
+                        if (event.type !== "on_round_begin") {
+                            return;
+                        }
+                        const targets = selectConditionTargets(status.effects[effect].target, event.source.character, event.target, event.combatants);
+                        targets.forEach(target => {
+                            const activeStatus = target.getActiveStatusInstance(statusId);
+                            const damageToDeal = Decimal(status.effects[effect].value)
+                                .times(target.getStatusStacks(statusId))
+                                .times(getCharacter(activeStatus.source.character).powerLevel)
+                                .floor();
+                            target.dealDamage(damageToDeal);
+                            event.roundEvents.push(generateDamageEvent(getCharacter(activeStatus.source.character), target, damageToDeal, "status", statusId));
+                        })
+                        break;
+                }
+            })
         })
-    })
+    });
+
     _.get(event.source, ["attack", "enhancements"], []).forEach(enhancement => {
         const enhancementDef = ActionEnhancements[enhancement];
         const eventDefinition = enhancementDef[event.type];
@@ -60,16 +64,43 @@ export default function triggerEvent(event) {
             debugMessage(`Enhancement ${enhancement.id} did ${traitTriggered ? '' : 'not'} trigger.`);
             const effectsToApply = eventDefinition[traitTriggered ? "trigger_effects" : "not_trigger_effects"];
             if (effectsToApply) {
-                applyTraitEffects(effectsToApply, event, "enhancement", enhancement, event.source.character.powerLevel);
+                applyTraitEffects(effectsToApply, event.source.character, event, "enhancement", enhancement, event.source.character.powerLevel);
             }
         }
     });
 }
 
 const eventMatcher = JOI.object({
-    type: JOI.valid("on_kill", "on_taking_damage", "on_round_begin", "on_round_end", "on_miss", "on_hit", "on_minor_hit", "on_glancing_hit", "on_solid_hit", "on_serious_hit", "on_devastating_hit", "on_combat_start").required(),
+    type: JOI.valid("on_status_applied", "on_kill", "on_taking_damage", "on_round_begin", "on_round_end", "on_miss", "on_hit", "on_minor_hit", "on_glancing_hit", "on_solid_hit", "on_serious_hit", "on_devastating_hit", "on_combat_start").required(),
     combatants: JOI.object().required(),
     roundEvents: JOI.array().required(),
     source: JOI.object(),
-    target: JOI.object()
+    target: JOI.any().when("type", {
+        is: [
+            "on_status_applied_to_self",
+            "on_status_applied_to_enemy",
+            "on_kill",
+            "on_taking_damage",
+            "on_miss",
+            "on_hit",
+            "on_minor_hit",
+            "on_glancing_hit",
+            "on_solid_hit",
+            "on_serious_hit",
+            "on_devastating_hit"],
+        then: JOI.object(),
+        otherwise: JOI.any().strip()
+    }),
+    status: JOI.any().when("type", {
+        is: 'on_status_applied',
+        then: JOI.string().valid(...Object.keys(Statuses))
+    }),
+    stacks: JOI.any().when("type", {
+        is: 'on_status_applied',
+        then: JOI.alternatives().try(JOI.number(), JOI.object().instance(Decimal))
+    }),
+    duration: JOI.any().when("type", {
+        is: 'on_status_applied',
+        then: JOI.alternatives().try(JOI.number(), JOI.object().instance(Decimal))
+    })
 });
