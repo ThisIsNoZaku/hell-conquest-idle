3b829f2088b65b14b038cf5d47ec0b37de3c2342
Damie <damienmarble@gmail.com>
2021-02-05 11:41:48 -0500

    Used combined actions instead of attack only.

--- src/engine/combat/actions/calculateActionCost.js
+++ src/engine/combat/actions/calculateActionCost.js
@@ -1,13 +1,14 @@
 import {getConfigurationValue} from "../../../config";
 import calculateAttributeDifferentMultiplier from "../calculateAttributeDifferentMultiplier";
-import {AttackActions} from "../../../data/CombatActions";
+import {CombatActions} from "../../../data/CombatActions";
 import {Decimal} from "decimal.js";
 import * as _ from "lodash";
 import {ActionEnhancements} from "../../../data/ActionEnhancements";
+import {Traits} from "../../../data/Traits";
 export default function calculateActionCost(actor, action, target) {
     const base = Decimal(_.get(target, "powerLevel", 1)).times(getConfigurationValue("attack_upgrade_cost_per_enemy_level"));
     const attributeMultiplier =Decimal(1).minus(getConfigurationValue("mechanics.combat.precision.effectPerPoint")).pow(Decimal(_.get(actor, ["combat", "precision"], 1)));
-    const actionCostMultiplier = AttackActions[action.primary].energyCostMultiplier;
+    const actionCostMultiplier = CombatActions[action.primary].energyCostMultiplier;
     const enhancementModifier = action.enhancements.map(e => ActionEnhancements[e]).reduce((previousValue, currentValue, currentIndex) => {
         return previousValue + (currentValue.additional_energy_cost_modifier || 0);
     }, 0);
