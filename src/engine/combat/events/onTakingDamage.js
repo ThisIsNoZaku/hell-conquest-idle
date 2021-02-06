a877c4ab7ac6a5c6136f07d9c1fa1dd359d20e9c
Damie <damienmarble@gmail.com>
2021-02-05 20:24:35 -0500

    Method for on damage event.

---
+++ src/engine/combat/events/onTakingDamage.js
@@ -0,0 +1,18 @@
+import triggerEvent from "../../general/triggerEvent";
+
+export default function onTakingDamage(damagedCharacter, attackingCharacter, attack, damage, roundEvents) {
+    triggerEvent({
+        type: `on_taking_damage`,
+        source: {
+            character: damagedCharacter,
+            attack,
+            damage
+        },
+        target: attackingCharacter,
+        combatants: {
+            [attackingCharacter.id]: attackingCharacter,
+            [damagedCharacter.id]: damagedCharacter
+        },
+        roundEvents
+    });
+}
