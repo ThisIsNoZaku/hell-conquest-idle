ff6338744daa6da5604846ec2418486133b129af
Damie <damienmarble@gmail.com>
2021-02-06 21:28:38 -0500

    Removed some conditionals.

--- src/engine/combat/actions/resolveAction.js
+++ src/engine/combat/actions/resolveAction.js
@@ -6,33 +6,30 @@ import {CombatActions} from "../../../data/CombatActions";
 
 
 export default function resolveAction(actingCharacter, action, targetCharacter, reaction, roundEvents, tick) {
-    if (CombatActions[action.primary].attack) {
+    if(action.primary !== "none" || reaction.primary !== "none") {
         resolveAttack(actingCharacter, action, targetCharacter, reaction, roundEvents, tick);
-        switch (action.primary) {
-            case "none":
-                actingCharacter.initiative = 0;
-                break;
-            case "basicAttack":
-                actingCharacter.initiative -= 5;
-                break;
-            case "powerAttack":
-                actingCharacter.initiative -= 10;
-                break;
-        }
-        switch (reaction.primary) {
-            case "block":
-                actingCharacter.initiative -= 5;
-                targetCharacter.initiative -= 5;
-                break;
-            case "dodge":
-                actingCharacter.initiative -= 5;
-                targetCharacter.initiative += 10;
-                break;
-            case "none":
-                targetCharacter.initiative = 0;
-                break;
-        }
-    } else {
-        roundEvents.push(generateActionSkipEvent(actingCharacter, tick, "to save energy"));
+    }
+    switch (action.primary) {
+        case "none":
+            actingCharacter.initiative = 0;
+            break;
+        case "basicAttack":
+            actingCharacter.initiative -= 5;
+            break;
+        case "powerAttack":
+            actingCharacter.initiative -= 10;
+            break;
+    }
+    switch (reaction.primary) {
+        case "block":
+            actingCharacter.initiative -= 5;
+            targetCharacter.initiative -= 5;
+            break;
+        case "dodge":
+            targetCharacter.initiative += 10;
+            break;
+        case "none":
+            targetCharacter.initiative = 0;
+            break;
     }
 }
