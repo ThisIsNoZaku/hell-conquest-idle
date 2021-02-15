03d0b2b346e956574e38a64ec378e38cbd24a381
Damie <damienmarble@gmail.com>
2021-02-15 14:17:56 -0500

    Refactored validation for event triggers.

--- src/data/schemas/events.js
+++ src/data/schemas/events.js
@@ -1,3 +1,91 @@
-export const events = {
+import * as JOI from "joi";
+import {effectTarget, modifierEffects} from "./effects";
+import {Statuses} from "../Statuses";
 
-};
+const conditionTriggerTarget = JOI.valid("any_enemy");
+
+const statusNames = Object.keys(Statuses);
+
+// Conditions
+const eventConditions = JOI.object({
+    health_percentage: JOI.object({
+        target: conditionTriggerTarget,
+        below: [JOI.number().min(0).max(100), JOI.string()]
+    }),
+    chance: [JOI.number().min(0).max(100), JOI.string()],
+    source_character_is: JOI.object({
+        target: effectTarget
+    }),
+    target_character_is: JOI.object({
+        target: effectTarget
+    })
+});
+
+// Effects
+const eventEffects = modifierEffects.keys({
+    add_statuses: JOI.object().keys(statusNames.reduce((schemas, nextStatus) => {
+        schemas[nextStatus] = JOI.object({
+            target: effectTarget,
+            stacks: [JOI.string(), JOI.number().min(1)],
+            stacks_per_level: JOI.number().min(1),
+            duration: JOI.number().min(-1).default(1),
+            max: [JOI.number().min(1), JOI.string()],
+            cumulative: JOI.boolean()
+        });
+        return schemas;
+    }, {})),
+    remove_statuses: JOI.object().keys(statusNames.reduce((schemas, nextStatus) => {
+        schemas[nextStatus] = JOI.object({
+            target: effectTarget,
+            sourceTrait: JOI.string(),
+            stacks: [JOI.string(), JOI.number().min(0)]
+        });
+        return schemas;
+    }, {})),
+    reflect_statuses: JOI.object({
+        target: effectTarget,
+        value: JOI.number(),
+    }),
+    change_stamina: JOI.object({
+        target: effectTarget,
+        percentage_of_maximum_stamina: [JOI.string(), JOI.number()]
+    }),
+    change_health: JOI.object({
+        target: effectTarget,
+        percentage_of_maximum_health: [JOI.string(), JOI.number()],
+        value: [JOI.string(), JOI.number()]
+    }),
+    change_fatigue: JOI.object({
+        target: effectTarget,
+        percentage_of_maximum_stamina: [JOI.string(), JOI.number()],
+        value: [JOI.string(), JOI.number()]
+    }),
+    trait_mirror: JOI.object({
+        target: effectTarget,
+        value: JOI.number()
+    })
+});
+
+// Validators
+export const onEventValidator = JOI.object({
+    conditions: eventConditions,
+    trigger_effects: eventEffects,
+    not_trigger_effects: eventEffects
+});
+
+export const eventsValidator = JOI.object({
+    on_round_end: onEventValidator,
+    on_hit: onEventValidator,
+    on_intimidate: onEventValidator,
+    on_solid_hit: onEventValidator,
+    on_serious_hit: onEventValidator,
+    on_devastating_hit: onEventValidator,
+    on_glancing_hit: onEventValidator,
+    on_minor_hit: onEventValidator,
+    on_critical_hit: onEventValidator,
+    on_kill: onEventValidator,
+    on_taking_damage: onEventValidator,
+    on_combat_start: onEventValidator,
+    on_status_applied: onEventValidator,
+    on_dodge: onEventValidator
+});
--- src/data/schemas/traits.js
+++ src/data/schemas/traits.js
@@ -2,109 +2,21 @@ import * as JOI from "joi";
 import {Statuses} from "../Statuses";
 import {effectTarget, modifierEffects} from "./effects";
 import {Traits} from "../Traits";
-
-const conditionTriggerTarget = JOI.valid("any_enemy");
-
-const statusNames = Object.keys(Statuses);
-// Conditions
-const eventConditions = JOI.object({
-    health_percentage: JOI.object({
-        target: conditionTriggerTarget,
-        below: [JOI.number().min(0).max(100), JOI.string()]
-    }),
-    chance: [JOI.number().min(0).max(100), JOI.string()],
-    source_character_is: JOI.object({
-        target: effectTarget
-    }),
-    target_character_is: JOI.object({
-        target: effectTarget
-    })
-});
-
-// Effects
-const eventEffects = modifierEffects.keys({
-    add_statuses: JOI.object().keys(statusNames.reduce((schemas, nextStatus) => {
-        schemas[nextStatus] = JOI.object({
-            target: effectTarget,
-            stacks: [JOI.string(), JOI.number().min(1)],
-            stacks_per_level: JOI.number().min(1),
-            duration: JOI.number().min(-1).default(1),
-            max: [JOI.number().min(1), JOI.string()],
-            cumulative: JOI.boolean()
-        });
-        return schemas;
-    }, {})),
-    remove_statuses: JOI.object().keys(statusNames.reduce((schemas, nextStatus) => {
-        schemas[nextStatus] = JOI.object({
-            target: effectTarget,
-            sourceTrait: JOI.string(),
-            stacks: [JOI.string(), JOI.number().min(0)]
-        });
-        return schemas;
-    }, {})),
-    reflect_statuses: JOI.object({
-        target: effectTarget,
-        value: JOI.number(),
-    }),
-    change_stamina: JOI.object({
-        target: effectTarget,
-        percentage_of_maximum_stamina: [JOI.string(), JOI.number()]
-    }),
-    change_health: JOI.object({
-        target: effectTarget,
-        percentage_of_maximum_health: [JOI.string(), JOI.number()],
-        value: [JOI.string(), JOI.number()]
-    }),
-    trait_mirror: JOI.object({
-        target: effectTarget,
-        value: JOI.number()
-    })
-});
-
-// Validators
-const onEventValidator = JOI.object({
-    conditions: eventConditions,
-    trigger_effects: eventEffects,
-    not_trigger_effects: eventEffects
-});
-
-const onIntimidateValidator = onEventValidator;
-
-const onAttackHitValidator = onIntimidateValidator
-
-const onKillValidator = onAttackHitValidator;
+import {eventsValidator} from "./events";
 
 const continuousValidator = JOI.object({
     trigger_effects: modifierEffects,
     not_trigger_effects: modifierEffects
 });
 
-const onTakingDamageValidator = onAttackHitValidator;
-
-const onCombatStartValidator = onEventValidator;
-
-const traitValidator = JOI.object({
+const traitValidator = eventsValidator.keys({
     name: JOI.string().required(),
     icon: JOI.string().default("./icons/icons-1.png"),
     enabled: JOI.boolean(),
     attack_enhancement: JOI.string(),
     defense_enhancement: JOI.string(),
     description: JOI.function(),
-    on_round_end: onEventValidator,
-    on_hit: onAttackHitValidator,
-    on_intimidate: onIntimidateValidator,
-    on_solid_hit: onAttackHitValidator,
-    on_serious_hit: onAttackHitValidator,
-    on_devastating_hit: onAttackHitValidator,
-    on_glancing_hit: onAttackHitValidator,
-    on_minor_hit: onAttackHitValidator,
-    on_critical_hit: onAttackHitValidator,
-    on_kill: onKillValidator,
     continuous: continuousValidator,
-    on_taking_damage: onTakingDamageValidator,
-    on_combat_start: onCombatStartValidator,
-    on_status_applied: onEventValidator,
-    on_dodge: onEventValidator
 }).unknown(false);
 
 export function validatedTrait(object) {
