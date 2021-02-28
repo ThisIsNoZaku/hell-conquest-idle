eb720715d534e5d7415bb5ea15046664b6b1c259
Damie <damienmarble@gmail.com>
2021-02-27 22:57:30 -0500

    Faction icons and data.

---
+++ src/data/Factions.js
@@ -0,0 +1,32 @@
+export const Factions = {
+    alsharu: {
+        name: "The Alsharu",
+        icon: "./icons/Faction1.png",
+        bonus_stat: "cunning",
+        description: "Servants of Djinn"
+    },
+    waru: {
+        name: "The Waru",
+        icon: "./icons/Faction7.png",
+        bonus_stat: "deceit",
+        description: "Slaves of Oni"
+    },
+    kakoi: {
+        name: "The Kakoi",
+        icon: "./icons/Faction13.png",
+        bonus_stat: "madness",
+        description: "Subjects of Cyclops"
+    },
+    aterradora: {
+        name: "The Aterradora",
+        icon: "./icons/Faction4.png",
+        bonus_stat: "brutality",
+        description: "Minions of Jaguar"
+    }
+}
+
+export const ReputationLevels = {
+    good: "./icons/icons-2643.png",
+    neutral: "./icons/icons-2647.png",
+    bad: "./icons/icons-2644.png",
+}
