a7c9221c5dda3b884b8abfcba7d00327a2c34e13
Damie <damienmarble@gmail.com>
2021-02-27 22:52:00 -0500

    Redo fatigue.

--- src/engine/combat/events/onCombatRoundEnd.js
+++ src/engine/combat/events/onCombatRoundEnd.js
@@ -3,6 +3,7 @@ import {DURATION_FOR_COMBAT, DURATION_PERMANENT} from "../../../data/Statuses";
 import {generateFatigueDamageEvent, generateKillEvent, generateRemoveStatusEvent} from "../../events/generate";
 import {getCharacter} from "../../index";
 import {Decimal} from "decimal.js";
+import determineCharacterCombatAction from "../actions/determineCharacterCombatAction";
 
 export default function onCombatRoundEnd(combatants, roundEvents, tick) {
     const everyoneAlive = Object.values(combatants).every(c => c.isAlive);
@@ -40,18 +41,19 @@ export default function onCombatRoundEnd(combatants, roundEvents, tick) {
                 }
             });
             // Gain end-of-round Energy
-            const endOfRoundStaminaGain = Decimal.min(combatant.energyGeneration.times(tick - combatant.lastActedTick).floor(), combatant.combat.maximumStamina.minus(combatant.combat.stamina));
-            combatant.combat.fatigue = Decimal(combatant.combat.fatigue).plus(endOfRoundStaminaGain);
-            combatant.combat.stamina = Decimal.min(combatant.combat.stamina.plus(Decimal.max(endOfRoundStaminaGain, 0)), combatant.combat.maximumStamina);
-            const burnDamage = Decimal.max(0, combatant.combat.fatigue.minus(combatant.combat.maximumStamina).div(10)).floor();
-            if (burnDamage.gt(0)) {
-                roundEvents.push(generateFatigueDamageEvent(combatant, combatant, burnDamage));
-                combatant.dealDamage(burnDamage, "burn");
-                if (!combatant.isAlive) {
-                    roundEvents.push(generateKillEvent(combatant, combatant));
+            if(combatant.lastAction === "none") {
+                combatant.combat.fatigue = combatant.combat.fatigue.plus(1);
+                combatant.combat.stamina = combatant.combat.maximumStamina;
+                const burnDamage = combatant.maximumHp.div(10).times(combatant.combat.fatigue).floor();
+                if (burnDamage.gt(0)) {
+                    roundEvents.push(generateFatigueDamageEvent(combatant, combatant, burnDamage));
+                    combatant.dealDamage(burnDamage, "burn");
+                    if (!combatant.isAlive) {
+                        roundEvents.push(generateKillEvent(combatant, combatant));
+                    }R
                 }
+                combatant.lastActedTick = tick;
             }
-            combatant.lastActedTick = tick;
         });
     }
 }
