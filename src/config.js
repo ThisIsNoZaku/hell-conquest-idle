f8b0a51b35e2a0902d8cc350fdebc72dcaba94e7
Damie <damienmarble@gmail.com>
2021-03-19 19:57:41 -0400

    Modified configuration.

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
     greater_level_scale: 1, // A demon is "greater" than another when its level is this much higher.
     intimidateable_demon_level: 10, // A demon can be intimidated when its level is this much lower than the player
     enemy_latent_power: "Decimal.max(0, encounterLevel.minus(1).times(5))", // The amount of latent power enemy demons start with
-    bonus_points_for_highest_level : "highestLevelReached.plus(highestLevelReached.div(10).floor())", // How many bonus points the player gets, based on their highest level reached.
+    bonus_points_for_highest_level: "highestLevelReached.plus(highestLevelReached.div(10).floor())", // How many bonus points the player gets, based on their highest level reached.
     latent_power_cap: "highestLevelEnemyDefeated.plus(1).times(10)", // The maximum the player's latent power cap can be.
     lesser_demon_instant_kill_level: "highestLevelReached.minus(10)",
     latent_power_per_level: 1,
@@ -20,7 +20,6 @@ const config = {
     base_attack_downgrade_cost: 0,
     attack_upgrade_cost_per_enemy_level: 100, // Add this amount times enemy level to upgrade your attacks
     attack_downgrade_cost_per_enemy_level: 100, // Add this amount times enemy level to downgrade enemy attacks
-    damage_per_level: 15,
     flee_stamina_cost_base: 25,
     flee_stamina_minimum_cost: 1,
     instant_death_offset: 5,
@@ -36,82 +35,47 @@ const config = {
     maximum_downgrade_times: 1,
     fatigue_penalty_per_point: 25,
     stamina_recovery_per_level: 50,
-    base_power_generated_per_level_per_tick: 100,
+    base_power_generated_per_level_per_tick: 50,
     power_generation_growth_scaling: .01,
     game_level_cap: 51,
     good_reputation_cap: 15,
     good_reputation_threshold: 10,
     bad_reputation_threshold: -10,
     bad_reputation_floor: 15,
-    mechanics: {
-        reincarnation: {
-            latentPowerModifier: "Decimal.min(latentPower, highestLevelReached).plus(latentPower.minus(Decimal.min(latentPower, highestLevelReached).sqrt())",
-            traitPointCost: "Decimal.max(1, traitsOwned.times(2))",
-            attributePointCost: "Decimal.max(1, attributeScore)"
-        },
-        xp: {
-            gainedFromGreaterDemon: "Decimal(enemy.powerLevel).plus(1).pow(2).times(100)",
-            gainedFromLesserDemon: "Decimal(enemy.powerLevel).pow(2).times(10)",
-            gainedFromOtherDemon: "Decimal(enemy.powerLevel).pow(2).times(100)"
-        },
-        powerNeededForLevel: "level.eq(1) ? 0 : Decimal(level.minus(1)).pow(2).times(100)",
-        combat: {
-            startingHitLevel: -1,
-            fatigueDamageMultiplier: 0.25,
-            attackUpgradeBaseCost: 100,
-            incomingAttackDowngradeBaseCost: 100,
-            stolenPowerScale: 0.25,
-            randomEncounterChance: "player.powerLevel.div(10).floor().times(10)",
-            determineHit: "roll >= target ? 'hit' : 'miss'",
-            attributeDifferenceMultipliers: {
-                "-5": 0.2,
-                "-4" : .33,
-                "-3" : .5,
-                "-2" : .75,
-                "-1" : .8,
-                0: 1,
-                1: 1.25,
-                2: 1.5,
-                3: 2,
-                4: 3,
-                5: 5
-            },
-            effectiveAttributeCalculation: "baseAttribute",
-            resilience: {
-                baseAttribute: "madness",
-                effectPerPoint: .05
-            },
-            precision: {
-                baseAttribute: "deceit",
-                effectPerPoint: .15
-            },
-            evasion: {
-                baseAttribute: "cunning",
-                effectPerPoint: .15
-            },
-            power: {
-                baseAttribute: "brutality",
-                effectPerPoint: .05
-            },
-            traitRank: {
-                baseAttribute: "madness",
-                effectPerPoint: .1
-            },
-            playerAttributeMinimum: 1,
-            fatigue: {
-                evasionPenaltyPerPoint: 2
-            },
-            hp: {
-                pcBonus: 25,
-                baseHp: 0,
-                pointsPerLevel: 50,
-                healingPercentage: .5,
-                baseAttribute: "brutality",
-                effectPerPoint: .1
-            },
-        }
+    health_per_level: 25,
+    damage_per_level: 5,
+    health_modifier_attribute: "madness",
+    attribute_health_modifier_scale: .1,
+    attribute_score_minimum: 1,
+    attribute_difference_multipliers: {
+        "-5": 0.2,
+        "-4": .33,
+        "-3": .5,
+        "-2": .75,
+        "-1": .8,
+        0: 1,
+        1: 1.25,
+        2: 1.5,
+        3: 2,
+        4: 3,
+        5: 5
     },
-    // Artifacts
+
+    // Combat
+    action_cost_per_enemy_level: 100,
+
+    // Combat Attributes
+    precision_base_attribute: "deceit", // The attribute used to calculate precision
+    resilience_base_attribute: "madness",
+    evasion_base_attribute: "cunning",
+    power_base_attribute: "brutality",
+
+    // Combat attribute effects
+    evasion_effect_per_point: 0.05,
+    precision_effect_per_point: 0.05,
+
+    energy_cost_per_enemy_level: 100, // Enemy level times this amount is the base action cost.
+
     artifacts_enabled: false,
 
     // Negotiation
@@ -122,7 +86,7 @@ const config = {
 
 export function getConfigurationValue(path, defaultValue) {
     const configValue = _.get(config, path);
-    if(configValue === undefined && defaultValue === undefined) {
+    if (configValue === undefined && defaultValue === undefined) {
         throw new Error(`No configuration value for ${path} found and no default found`);
     }
     return configValue;
