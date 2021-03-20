163af49d92658a7756a7b6edbc33541dd2d22db7
Damie <damienmarble@gmail.com>
2021-03-19 20:03:50 -0400

    Updated attribute descriptions.

--- src/data/Attributes.js
+++ src/data/Attributes.js
@@ -9,17 +9,17 @@ export const Attributes = {
     },
     cunning: {
         label: "Cunning",
-            description: _.template("Cunning is how quick thinking a Demon is. It adds to Evasion."),
+            description: _.template("Cunning is how quick thinking a Demon is. It adds to Evasion, helps hide your action from the enemy and increases energy gain per round."),
             icon: "icons/icons-24.png"
     },
     deceit: {
         label: "Deceit",
-            description: _.template("Deceit is how underhanded and manipulative a Demon is. It adds to Precision."),
+            description: _.template("Deceit is how underhanded and manipulative a Demon is. It adds to Precision and helps you figure out what action the enemy will perform."),
             icon: "icons/icons-17.png"
     },
     madness: {
         label: "Madness",
-            description: _.template(`Madness is how disconnected from the limits of reality the Demon is. It adds to Resilience and gives a \${tier * ${ getConfigurationValue("mechanics.combat.traitRank.effectPerPoint") * 100}}% bonus to the level of Traits, rounded down.`),
+            description: _.template(`Madness is how disconnected from the limits of reality the Demon is. It adds to Resilience and increases the level of your Traits when high enough.`),
             icon: "icons/icons-124.png"
     }
 }
